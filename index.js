6e95226e72fcb1fbab794e860651fd9c9829463a
Shimazaky80 <64324142+Shimazaky80@users.noreply.github.com>
2023-10-03 22:02:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// method chaining = calling one method after another
+//                   in one continuous line of acode
+
+let userName = "Shimazaky"
+
+let letter = userName.charAt(0).toUpperCase..trim()
+
+console.log(letter)
